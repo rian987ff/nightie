c6b462825d45962aa093bb8daa6d7f68fb8c078a
Rian Antônio <rian@MacBook-M4-de-Rian.local>
2025-10-06 00:43:16 -0300

    feat(performance): Converte imagens para WebP e otimiza assets

--- constants.ts
+++ constants.ts
@@ -1,40 +1,40 @@
-import img_23_1_6526_jpeg from "./src/assets/images/23-1_6526.jpeg";
-import img_23_10_jpeg from "./src/assets/images/23-10.jpeg";
-import img_23_11_jpeg from "./src/assets/images/23-11.jpeg";
-import img_23_12_jpeg from "./src/assets/images/23-12.jpeg";
-import img_23_13_jpeg from "./src/assets/images/23-13.jpeg";
-import img_23_14_jpeg from "./src/assets/images/23-14.jpeg";
-import img_23_15_jpeg from "./src/assets/images/23-15.jpeg";
-import img_23_16_jpeg from "./src/assets/images/23-16.jpeg";
-import img_23_17_jpg from "./src/assets/images/23-17.jpg";
-import img_23_18_jpeg from "./src/assets/images/23-18.jpeg";
-import img_23_19_jpeg from "./src/assets/images/23-19.jpeg";
-import img_23_2_jpeg from "./src/assets/images/23-2.jpeg";
-import img_23_20_jpeg from "./src/assets/images/23-20.jpeg";
-import img_23_22_jpeg from "./src/assets/images/23-22.jpeg";
-import img_23_23_jpeg from "./src/assets/images/23-23.jpeg";
-import img_23_24_jpeg from "./src/assets/images/23-24.jpeg";
-import img_23_25_jpeg from "./src/assets/images/23-25.jpeg";
-import img_23_26_jpeg from "./src/assets/images/23-26.jpeg";
-import img_23_27_jpeg from "./src/assets/images/23-27.jpeg";
-import img_23_28_jpeg from "./src/assets/images/23-28.jpeg";
-import img_23_29_jpeg from "./src/assets/images/23-29.jpeg";
-import img_23_3_jpeg from "./src/assets/images/23-3.jpeg";
-import img_23_31_jpeg from "./src/assets/images/23-31.jpeg";
-import img_23_32_jpeg from "./src/assets/images/23-32.jpeg";
-import img_23_4_jpeg from "./src/assets/images/23-4.jpeg";
-import img_23_5_jpeg from "./src/assets/images/23-5.jpeg";
-import img_23_6_jpeg from "./src/assets/images/23-6.jpeg";
-import img_23_7_jpeg from "./src/assets/images/23-7.jpeg";
-import img_23_8_jpeg from "./src/assets/images/23-8.jpeg";
-import img_23_9_jpeg from "./src/assets/images/23-9.jpeg";
-import img_Capa70s_jpg from "./src/assets/images/Capa70s.jpg";
-import img_CapaChill_png from "./src/assets/images/CapaChill.png";
-import img_CapaLibertei_png from "./src/assets/images/CapaLibertei.png";
-import img_CapaLov_jpg from "./src/assets/images/CapaLov.jpg";
-import img_CapaReggae_jpg from "./src/assets/images/CapaReggae.jpg";
-import img_Rock_jpg from "./src/assets/images/Rock.jpg";
-import img_carro_0860_jpg from "./src/assets/images/carro_0860.jpg";
+import img_23_1_6526_webp from "./src/assets/images/23-1_6526.webp";
+import img_23_10_webp from "./src/assets/images/23-10.webp";
+import img_23_11_webp from "./src/assets/images/23-11.webp";
+import img_23_12_webp from "./src/assets/images/23-12.webp";
+import img_23_13_webp from "./src/assets/images/23-13.webp";
+import img_23_14_webp from "./src/assets/images/23-14.webp";
+import img_23_15_webp from "./src/assets/images/23-15.webp";
+import img_23_16_webp from "./src/assets/images/23-16.webp";
+import img_23_17_webp from "./src/assets/images/23-17.webp";
+import img_23_18_webp from "./src/assets/images/23-18.webp";
+import img_23_19_webp from "./src/assets/images/23-19.webp";
+import img_23_2_webp from "./src/assets/images/23-2.webp";
+import img_23_20_webp from "./src/assets/images/23-20.webp";
+import img_23_22_webp from "./src/assets/images/23-22.webp";
+import img_23_23_webp from "./src/assets/images/23-23.webp";
+import img_23_24_webp from "./src/assets/images/23-24.webp";
+import img_23_25_webp from "./src/assets/images/23-25.webp";
+import img_23_26_webp from "./src/assets/images/23-26.webp";
+import img_23_27_webp from "./src/assets/images/23-27.webp";
+import img_23_28_webp from "./src/assets/images/23-28.webp";
+import img_23_29_webp from "./src/assets/images/23-29.webp";
+import img_23_3_webp from "./src/assets/images/23-3.webp";
+import img_23_31_webp from "./src/assets/images/23-31.webp";
+import img_23_32_webp from "./src/assets/images/23-32.webp";
+import img_23_4_webp from "./src/assets/images/23-4.webp";
+import img_23_5_webp from "./src/assets/images/23-5.webp";
+import img_23_6_webp from "./src/assets/images/23-6.webp";
+import img_23_7_webp from "./src/assets/images/23-7.webp";
+import img_23_8_webp from "./src/assets/images/23-8.webp";
+import img_23_9_webp from "./src/assets/images/23-9.webp";
+import img_Capa70s_webp from "./src/assets/images/Capa70s.webp";
+import img_CapaChill_webp from "./src/assets/images/CapaChill.webp";
+import img_CapaLibertei_webp from "./src/assets/images/CapaLibertei.webp";
+import img_CapaLov_webp from "./src/assets/images/CapaLov.webp";
+import img_CapaReggae_webp from "./src/assets/images/CapaReggae.webp";
+import img_Rock_webp from "./src/assets/images/Rock.webp";
+import img_carro_0860_webp from "./src/assets/images/carro_0860.webp";
 
 export const products = [
   {
@@ -85,52 +85,52 @@ export const playlists = [
   {
     description: "Fleetwood Mac, Evinha, Roberta Flack, Tim Maia e muitos mais",
     title: "70s Mix",
-    src: img_Capa70s_jpg,
+    src: img_Capa70s_webp,
     ctaText: "Listen now",
     ctaLink: "https://open.spotify.com/playlist/5cfqQnNJ2x1f4Rdjp9XNw8",
   },
   {
     description: "Bob Marley & The Wailers, Mato Seco, Soja, Gaia Piá entre outros",
     title: "Reggae Music",
-    src: img_CapaReggae_jpg,
+    src: img_CapaReggae_webp,
     ctaText: "Listen now",
     ctaLink: "https://open.spotify.com/playlist/3hLQlF8upMg2TYRQZ9efxS",
   },
         {
     description: "Red Hot Chili Peppers, The Strokes, Wings, U2, Radiohead, Pearl Jam, Hoobastank e mais",
     title: "Smooth Rock",
-    src: img_Rock_jpg,
+    src: img_Rock_webp,
     ctaText: "Listen now",
     ctaLink: "https://open.spotify.com/playlist/4g5ivFndhbIx0YCnYfc6Ei",
   },
   {
     description: "Rita Lee, Cassiano, Di Melo, Tim Maia, Trio Ternura, Sandra de Sá e muito mais",
     title: "Me Libertei",
-    src: img_CapaLibertei_png,
+    src: img_CapaLibertei_webp,
     ctaText: "Listen now",
     ctaLink: "https://open.spotify.com/track/2Fxmhks0bxGSBdJ92vM42m",
   },
   {
     description: " Sade, Frank Ocean, Amy Winehouse, Gotye, Damien Rice, Chris Isaak entre outros.",
     title: "Love Songs",
-    src: img_CapaLov_jpg,
+    src: img_CapaLov_webp,
     ctaText: "Listen now",
     ctaLink: "hhttps://open.spotify.com/playlist/3gRHmtmQfItZafh5jdmmc8",
   },
     {
     description: "The Cure, TSHA, Milky Chance, Juice WRLD, Billie Eilish, Dr. Dre e mais",
     title: "Chill Out",
-    src: img_CapaChill_png,
+    src: img_CapaChill_webp,
     ctaText: "Listen now",
     ctaLink: "https://open.spotify.com/playlist/4CeniohOsuMduz3XIGegnh",
   },
 ];
 
 export const lookbookSliderImages = [
-  img_23_18_jpeg,
-  img_23_1_6526_jpeg,
-  img_23_2_jpeg,
-  img_carro_0860_jpg,
+  img_23_18_webp,
+  img_23_1_6526_webp,
+  img_23_2_webp,
+  img_carro_0860_webp,
   
 ];
 
@@ -138,151 +138,151 @@ export const lookbookImages = [
   {
     id: 1,
     className: "",
-    thumbnail: img_23_1_6526_jpeg,
+    thumbnail: img_23_1_6526_webp,
   },
   {
     id: 2,
     className: "",
-    thumbnail: img_23_16_jpeg,
+    thumbnail: img_23_16_webp,
   },
   {
     id: 3,
     className: "",
-    thumbnail: img_23_23_jpeg,
+    thumbnail: img_23_23_webp,
   },
   {
     id: 4,
     className: "",
-    thumbnail: img_23_10_jpeg,
+    thumbnail: img_23_10_webp,
   },
   {
     id: 5,
     className: "",
-    thumbnail: img_23_13_jpeg,
+    thumbnail: img_23_13_webp,
   },
   {
     id: 6,
     className: "",
-    thumbnail: img_23_12_jpeg,
+    thumbnail: img_23_12_webp,
   },
   {
     id: 7,
     className: "",
-    thumbnail: img_23_11_jpeg,
+    thumbnail: img_23_11_webp,
   },
   {
     id: 8,
     className: "",
-    thumbnail: img_23_24_jpeg,
+    thumbnail: img_23_24_webp,
   },
   {
     id: 9,
     className: "",
-    thumbnail: img_23_29_jpeg,
+    thumbnail: img_23_29_webp,
   },
   {
     id: 10,
     className: "",
-    thumbnail: img_23_8_jpeg,
+    thumbnail: img_23_8_webp,
   },
   {
     id: 11,
     className: "",
-    thumbnail: img_23_9_jpeg,
+    thumbnail: img_23_9_webp,
   },
   {
     id: 12,
     className: "",
-    thumbnail: img_23_31_jpeg,
+    thumbnail: img_23_31_webp,
   },
   {
     id: 13,
     className: "",
-    thumbnail: img_23_20_jpeg,
+    thumbnail: img_23_20_webp,
   },
   {
     id: 14,
     className: "",
-    thumbnail: img_23_32_jpeg,
+    thumbnail: img_23_32_webp,
   },
   {
     id: 15,
     className: "",
-    thumbnail: img_23_5_jpeg,
+    thumbnail: img_23_5_webp,
   },
   {
     id: 16,
     className: "",
-    thumbnail: img_23_19_jpeg,
+    thumbnail: img_23_19_webp,
   },
   {
     id: 17,
     className: "",
-    thumbnail: img_23_7_jpeg,
+    thumbnail: img_23_7_webp,
   },
   {
     id: 18,
     className: "",
-    thumbnail: img_23_28_jpeg,
+    thumbnail: img_23_28_webp,
   },
   {
     id: 19,
     className: "",
-    thumbnail: img_carro_0860_jpg,
+    thumbnail: img_carro_0860_webp,
   },
   {
     id: 20,
     className: "",
-    thumbnail: img_23_2_jpeg,
+    thumbnail: img_23_2_webp,
   },
   {
     id: 21,
     className: "",
-    thumbnail: img_23_22_jpeg,
+    thumbnail: img_23_22_webp,
   },
   {
     id: 22,
     className: "",
-    thumbnail: img_23_27_jpeg,
+    thumbnail: img_23_27_webp,
   },
   {
     id: 23,
     className: "",
-    thumbnail: img_23_4_jpeg,
+    thumbnail: img_23_4_webp,
   },
   {
     id: 24,
     className: "",
-    thumbnail: img_23_6_jpeg,
+    thumbnail: img_23_6_webp,
   },
   {
     id: 25,
     className: "",
-    thumbnail: img_23_3_jpeg,
+    thumbnail: img_23_3_webp,
   },
   {
     id: 26,
     className: "",
-    thumbnail: img_23_15_jpeg,
+    thumbnail: img_23_15_webp,
   },
   {
     id: 27,
     className: "",
-    thumbnail: img_23_14_jpeg,
+    thumbnail: img_23_14_webp,
   },
   {
     id: 28,
     className: "",
-    thumbnail: img_23_26_jpeg,
+    thumbnail: img_23_26_webp,
   },
   {
     id: 29,
     className: "",
-    thumbnail: img_23_17_jpg,
+    thumbnail: img_23_17_webp,
   },
   {
     id: 30,
     className: "",
-    thumbnail: img_23_25_jpeg,
+    thumbnail: img_23_25_webp,
   },
 ];
